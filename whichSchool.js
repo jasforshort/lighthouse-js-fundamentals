51051c265e71b888f421e4dc190ae45c4fe0927a
Jas Mann <jaforshort@hotmail.ca>
2022-05-04 22:17:54 +0000

    Conditionals and Looping exercies

---
+++ whichSchool.js
@@ -0,0 +1,17 @@
+// function that gets an age and returns what school to go to based on age
+const whichSchool = function (age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else
+  return "Lighthouse Labs";
+}
+
+// logs of different ages
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
