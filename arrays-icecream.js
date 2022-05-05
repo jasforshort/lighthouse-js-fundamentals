2008a2109ef213409ab2431ff3bea0916f444584
Jas Mann <jaforshort@hotmail.ca>
2022-05-05 15:05:45 +0000

    Working with arrays and ice cream, yum

---
+++ arrays-icecream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// adding root beer using push
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+// getting the first index or flavour of the array
+console.log(iceCreamFlavours[0]);
+
+// getting the last index or flavour of the array
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// gets the total number of flavours
+console.log(iceCreamFlavours.length);
